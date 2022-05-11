b24991d4db91299366549f8ef16c0b8571a33fa3
somu <somnath553.in@gmail.com>
2022-05-12 02:33:36 +0530

    my react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import ToggleButton from "./Components/ToggleButton";
+import React, { useState } from "react";
 
 function App() {
+  
+  const [on,off]=useState(0);
+  const [navl,navd]=useState("light");
+  const [tostt,setostt]=useState("colored");
+  
+  const tog=()=>{
+    if(on===0)
+    {
+      off(1);
+      navd("dark");
+      setostt("dark");
+      document.querySelector("body").style.backgroundColor ="#181a1b";
+    }
+    else{
+      off(0);
+      navd("light");
+      setostt("colored");
+      document.querySelector("body").style.backgroundColor ="white";
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="cover">
+    
+      <Navbar title="My ReactApp" value={"1"} togf={navl} />
+      <div className="container covers">
+        <TextForm heading="My text area" togf={navl} tost={tostt} />
+      </div>
+      
+      <ToggleButton onChange={tog} className="tg"  />
+      
+      
+      </div>
   );
 }
 
 export default App;
+ 
---
+++ src/Components/TextForm.js
@@ -0,0 +1,127 @@
+// import { useState } from "react/cjs/react.production.min"
+import React, { useState } from "react";
+// import {toast} from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+export default function TextForm(props) {
+  // toast.configure()
+
+  const handle = (event) => {
+    k(t1);
+    t2(event.target.value);
+  };
+  const toUpper = () => {
+    toast.success("Sucessfully converted to uppercase" , {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    t2(t1.toUpperCase());
+  };
+  const toLower = () => {
+    toast.success("Sucessfully converted to lower case", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    t2(t1.toLowerCase());
+  };
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = t1;
+    window.speechSynthesis.speak(msg);
+    toast.success("Speaking", {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+  };
+  const rmspace = () => {
+    k(t1);
+    let newt=t1.split(/[  ]+/);
+    t2(newt.join(" "));
+    toast.info(" Extra space removed", {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+    
+  };
+  const copy = () => {
+    navigator.clipboard.writeText(t1);
+    if (t1 !== "") {
+      toast.info("text copied:: " + t1, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    }
+  };
+  const Undo = () => {
+    toast.warn("Undo", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    console.log(j);
+    t2(j);
+  };
+  const Clear = () => {
+    k(t1);
+    if (t1 !== "") {
+      toast.error("text cleared", { position: toast.POSITION.BOTTOM_RIGHT });
+    }
+
+    t2("");
+  };
+  const [t1, t2] = useState("text here");
+  const [j, k] = useState("text here");
+
+  return (
+    <>
+    
+      <div className="container">
+      <h1 className={`${props.togf}`}>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className={`form-control forf ${props.togf}`}
+          id="mybox"
+          value={t1}
+          onChange={handle}
+          rows="5"
+        ></textarea>
+      </div>
+      </div>
+      <div className="container ct">
+      <button className="btn btn-outline-success" onClick={toUpper}>
+        To upper case
+      </button>
+      <button className="btn btn-outline-success" onClick={toLower}>
+        To lower case
+      </button>
+      <button className="btn btn-outline-success" onClick={speak}>
+        Speak
+      </button>
+      <button className="btn btn-outline-info" onClick={rmspace}>
+        Remove extra space
+      </button>
+      <button className="btn btn-outline-info" onClick={copy}>
+        copy
+      </button>
+      <button className="btn btn-outline-warning" onClick={Undo}>
+        Undo
+      </button>
+      <button className="btn btn-outline-danger" onClick={Clear}>
+        Clear
+      </button>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        theme={`${props.tost}`}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover="true"
+      />
+      </div>
+      <div className="container my-3">
+        <h1 className={`${props.togf}`}>Text Summary</h1>
+        <p className={`${props.togf}`} >{t1.split(' ').length-1} words {t1.length}characters</p>
+        <h2 className={`${props.togf}`}>Preview</h2>
+        <p className={`${props.togf}`}>{t1}</p>
+      </div>
+      
+    </>
+  );
+}
